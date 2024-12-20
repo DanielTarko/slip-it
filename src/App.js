11c079eca425edd0bd6a60664f57d665b08d3174
Daniel Tarko <dtarko@Daniels-MacBook-Air-171.local>
2024-12-20 13:07:15 +0100

    route fixing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
 
 import Retailer from './pages/Retailer';
@@ -11,14 +11,12 @@ import CreateAccountPage from './pages/CreateAccountPage';
 function App() {
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LogInPage />} />
-        <Route path="/create-account" element={<CreateAccountPage />} />
-        <Route path="/retailer" element={<Retailer />} />
-        <Route path="*" element={<NotFoundPage />} /> {/* Catch-all for 404 */}
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<LogInPage />} />
+      <Route path="/create-account" element={<CreateAccountPage />} />
+      <Route path="/retailer" element={<Retailer />} />
+      <Route path="*" element={<NotFoundPage />} /> {/* Catch-all for 404 */}
+    </Routes>
   );
 }
 
